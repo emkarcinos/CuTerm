98826e604403fb99db2902122afbe8f9c1bdf445
Zenbook <komar86311@gmail.com>
2019-05-25 18:34:50 +0200

    added tooltip

--- include/Window.h
+++ include/Window.h
@@ -2,7 +2,9 @@
 
 class Window {
     private:
+        /* sets custom border */
         virtual void setFrame(const char &frameChar)=0;
+        /* adds text to the top of the window */
         virtual void setTitle(const char* winTitle)=0;
-        
+
 };
