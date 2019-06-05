ff6a4ba5e4f5009abc3649fdaa03cf7bc042a384
Zenbook <komar86311@gmail.com>
2019-06-05 21:37:35 +0200

    added format function declaration

--- include/TextObject.h
+++ include/TextObject.h
@@ -6,16 +6,19 @@
 #define MAX_STR_LINES 256
 
 class TextObject {
-    private:
         struct Dimmensions {
             unsigned short x;
             unsigned short y;
         } size;
+        /* Raw text */
         std::string text;
+        /* Parsed text */
         std::string textArray[MAX_STR_LINES];
     public:
         /* Returns a textbox with default dimmensions x=20, y=5 */
         TextObject();
-        /* Returns a textbox with given dimmensions and optionally a text to format */
-        TextObject(const unsigned int x, const unsigned int y, std::string _text=NULL);
+        /* Returns a textbox with given dimmensions and a text to format */
+        TextObject(const unsigned int x, const unsigned int y, const std::string text);
+        /* Parses a string to a 2D string array, centered or justified and with a given spacing */
+        void format(const std::string text, const std::string method, const unsigned int spacing=0);
 };
