34cf8732e098a9dc05aad421602356e2535ae543
Zenbook <komar86311@gmail.com>
2019-06-06 08:59:53 +0200

    added TextObject.cpp

---
+++ src/TextObject.cpp
@@ -0,0 +1,35 @@
+#include "TextObject.h"
+
+void TextObject::splitStringToLines(const std::string text){
+
+}
+
+std::string TextObject::alignLineCenter(const std::string text){
+
+}
+
+std::string TextObject::alignLineLeft(const std::string text){
+
+}
+
+std::string TextObject::alignLineRight(const std::string text){
+
+}
+
+TextObject::TextObject(){
+    
+}
+
+TextObject::TextObject(const unsigned int x, const unsigned int y, const std::string text){
+    
+}
+
+void TextObject::appendString(const std::string text){
+
+}
+
+void format(const std::string text, const std::string method, const unsigned int spacing=0){
+
+}
+
+
