6e67cb82a5cf0280f8b2f0dae76a0f02b312b4f2
Zenbook <komar86311@gmail.com>
2019-06-08 22:33:22 +0200

    Added PromptWindow.cpp

---
+++ src/PromptWindow.cpp
@@ -0,0 +1,32 @@
+#include "PromptWindow.h"
+#include "TextObject.h"
+
+template<class type>
+void PromptWindow<type>::moveCursor(){
+
+}
+
+template<class type>
+void PromptWindow<type>::drawSeparator(){
+
+}
+
+template<class type>
+PromptWindow<type>::PromptWindow(const TextObject& textObj, const type& output, const char& frameChar, const char* winTitle){
+
+}
+
+template<class type>
+void PromptWindow<type>::addTextObject(TextObject& textObj){
+    
+}
+
+template<class type>
+void PromptWindow<type>::removeTextObject(TextObject& textObj){
+
+}
+
+template<class type>
+PromptWindow<type>::~PromptWindow(){
+
+}
