8963b74a454187cba6398d9cb65769ba387a55a8
Zenbook <komar86311@gmail.com>
2019-06-09 10:59:13 +0200

    Added PasswordPrompt.h

---
+++ include/PasswordPrompt.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "InnerWindow.h"
+
+#include <string>
+
+class PasswordPrompt : public InnerWindow {
+    private:
+        /* Obfuscated */
+        PasswordPrompt();
+        /* Adds "Password:"" to the window */
+        void addPassText();
+        /* Moves the cursor to the typing position */
+        void moveCursor();
+        /* Draws a line that separates the text from the prompt */
+        void drawSeparator();
+    public:
+        /* Returns a PasswordPrompt */
+       PasswordPrompt(const char& frameChar=' ', const char* winTitle="");
+       /* Input/output mechanism */
+        void prompt(std::string output);
+};
