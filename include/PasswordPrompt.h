bb057331a9b5292621479871bd536fb12c2569e0
Zenbook <komar86311@gmail.com>
2019-06-09 11:13:46 +0200

    Added virtual methods

--- include/PasswordPrompt.h
+++ include/PasswordPrompt.h
@@ -4,6 +4,8 @@
 
 #include <string>
 
+class TextObject;
+
 class PasswordPrompt : public InnerWindow {
     private:
         /* Obfuscated */
@@ -13,11 +15,17 @@ class PasswordPrompt : public InnerWindow {
         /* Moves the cursor to the typing position */
         void moveCursor();
         /* Draws a line that separates the text from the prompt */
-        void drawSeparator();
+        void drawPromptBox();
+        /* Obfuscated */
+        virtual void addTextObject(TextObject& obj) override;
+        /* Obfuscated */
+        virtual void removeTextObject(TextObject& textObj) override;
     public:
         /* Returns a PasswordPrompt */
         PasswordPrompt(const char& frameChar=' ', const char* winTitle="");
         /* Input/output mechanism */
         void prompt(std::string output);
+        /* Clears the matrix */
+        virtual void clear() override;
         virtual ~PasswordPrompt();
 };
