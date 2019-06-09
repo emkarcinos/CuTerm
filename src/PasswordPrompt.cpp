20dd251e54f400272221a6626dfbcb504518f9e4
Zenbook <komar86311@gmail.com>
2019-06-09 11:02:25 +0200

    Added PasswordPrompt.cpp

---
+++ src/PasswordPrompt.cpp
@@ -0,0 +1,26 @@
+#include "PasswordPrompt.h"
+#include "TerminalWindow.h"
+
+void PasswordPrompt::addPassText(){
+
+}
+
+void PasswordPrompt::moveCursor(){
+
+}
+
+void PasswordPrompt::drawSeparator(){
+
+}
+
+PasswordPrompt::PasswordPrompt(const char& frameChar, const char* winTitle){
+
+}
+
+void PasswordPrompt::prompt(std::string output){
+
+}
+
+PasswordPrompt::~PasswordPrompt(){
+    
+}
